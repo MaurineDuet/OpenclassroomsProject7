cb48619ef1d57caff5e5017dd5c3b81eadc58c48
Maurine Duet <duet_maurine@MacBook-Air-de-Maurine.local>
2023-06-01 18:49:53 +0200

    Add du système d'authentification

--- routes/library.js
+++ routes/library.js
@@ -1,16 +1,18 @@
 const express = require('express')
+const auth = require('../middleware/auth')
+
 const router = express.Router()
 
 const libraryCtrl = require('../controllers/library')
 
-router.post('/', libraryCtrl.createBook)
+router.post('/', auth, libraryCtrl.createBook)
 
 router.get('/', libraryCtrl.getAllBooks);
 
 router.get('/:id', libraryCtrl.getOneBook);
 
-router.put('/:id', libraryCtrl.modifyBook);
+router.put('/:id', auth, libraryCtrl.modifyBook);
 
-router.delete('/:id', libraryCtrl.deleteBook)
+router.delete('/:id', auth, libraryCtrl.deleteBook)
 
 module.exports = router
