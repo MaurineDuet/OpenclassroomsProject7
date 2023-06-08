14086e948b3b7464da2f3bf4768aab19c881c0a1
Maurine Duet <duet_maurine@MacBook-Air-de-Maurine.local>
2023-06-08 14:10:11 +0200

    Ajout de la partie multer

--- routes/library.js
+++ routes/library.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const auth = require('../middleware/auth')
+const multer = require('../middleware/multer-config')
 
 const router = express.Router()
 
 const libraryCtrl = require('../controllers/library')
 
-router.post('/', auth, libraryCtrl.createBook)
+router.post('/', auth, multer, libraryCtrl.createBook)
 
 router.get('/', libraryCtrl.getAllBooks);
 
 router.get('/:id', libraryCtrl.getOneBook);
 
-router.put('/:id', auth, libraryCtrl.modifyBook);
+router.put('/:id', auth, multer, libraryCtrl.modifyBook);
 
 router.delete('/:id', auth, libraryCtrl.deleteBook)
 
